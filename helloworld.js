9022f0089e1ba969c0f692a9433696d3acf7fe14
Marco <mornsophannamis@gmail.com>
2023-11-03 08:43:41 +0700

    hello world

---
+++ helloworld.js
@@ -0,0 +1,69 @@
+var helloworldjs;
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ src),
+  "sayHelloWorld": () => (/* reexport */ sayHelloWorld),
+  "sayHi": () => (/* reexport */ sayHi),
+  "sayHowAu": () => (/* reexport */ sayHowAu)
+});
+
+;// CONCATENATED MODULE: ./src/greeting.helper.ts
+function sayHelloWorld() {
+   console.log('Hello World!')
+}
+
+function sayHi() {
+   console.log('Hi!!!');
+}
+
+function sayHowAu() {
+   console.log('How are you?');
+}
+;// CONCATENATED MODULE: ./src/index.ts
+
+
+
+/* harmony default export */ const src = ({ sayHelloWorld: sayHelloWorld, sayHi: sayHi, sayHowAu: sayHowAu });
+
+helloworldjs = __webpack_exports__;
+/******/ })()
+;
